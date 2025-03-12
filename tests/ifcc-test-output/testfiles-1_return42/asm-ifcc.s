87bdb408482841252ee5c92260736883ffa5acce
AdSchl2E <adam.schlee@insa-lyon.fr>
2025-03-12 08:53:18 +0100

    INIT - Gitignore V2

--- tests/ifcc-test-output/testfiles-1_return42/asm-ifcc.s
+++
@@ -1,8 +0,0 @@
-Table des symboles :
-.globl main
- main: 
-    pushq %rbp
-    movq %rsp, %rbp
-    movl $42, %eax
-    popq %rbp
-    ret
--- tests/ifcc-test-output/testfiles-3_decl_var/asm-ifcc.s
+++
@@ -1,9 +0,0 @@
-Table des symboles :
-  a -> 0
-.globl main
- main: 
-    pushq %rbp
-    movq %rsp, %rbp
-    movl $42, %eax
-    popq %rbp
-    ret
